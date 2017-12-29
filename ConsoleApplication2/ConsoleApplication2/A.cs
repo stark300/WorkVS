8aeb668baddd2d0fb6f418934d90e21c685f5275
Sandeep Addala <sandy.addala1996@gmail.com>
2017-12-29 19:10:23 +0530

    added passing ref

---
+++ ConsoleApplication2/ConsoleApplication2/A.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class A
+    {
+        public void method(int i,out Program obj1, ref Program obj2)
+        {
+
+            obj1 = new Program();
+            obj1.i = 50;
+            obj2 = new Program();
+            obj2.i = 50;
+        }
+    }
+}
---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        public int i = 100;
+        static void Main(string[] args)
+        {
+            int i = 10;
+            Program obj1 ;
+            Program obj2 = new Program();
+            //obj1.i = 25;
+            A a = new A();
+            Console.WriteLine("Before  and " + obj2.i);
+            Console.WriteLine(" obj 1 : obj2 : "+obj2.GetHashCode());
+            a.method(i,out obj1, ref obj2);
+
+            Console.WriteLine("After " + obj1.i + " and " + obj2.i);
+            Console.WriteLine(" obj 1 : " + obj1.GetHashCode() + "obj2 : " + obj2.GetHashCode());
+            Console.ReadKey();
+        }
+    }
+}
